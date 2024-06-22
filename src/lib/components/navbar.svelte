4fb4c6c17d4687f7f3d9d0cedc8dc172606a1737
HangBeni <hangbenjamin2003@gmail.com>
2024-06-22 10:46:25 +0200

    nav comp

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  let links : Links = [
+    { name: "Főoldal", url: "/" },
+    { name: "Rólunk", url: "/about_us" },
+    { name: "Időpontfogalás", url: "/appointment" },
+    {name: "Itt születtem történeteket", url: "/i_birth_here"}
+  ];
+</script>
+
+
+<nav>
+  <h1>Melindák</h1>
+  {
+    links.map((link) => (
+      <a href={link.url}>{link.name}</a>
+    ))
+  } 
+</nav>
