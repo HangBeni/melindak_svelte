9904e2d1731af5e0dcc8ca1021eac544151877eb
HangBeni <hangbenjamin2003@gmail.com>
2024-07-08 16:00:34 +0200

    about us page with text

---
+++ src/routes/magunkrol/+page.svelte
@@ -0,0 +1,40 @@
+<h2>Právicz-Kárász Melinda</h2>
+
+<!-- Kárász Melinda képe Kárász Melinda és egy kisbaba -->
+
+<p>
+  ülésznő vagyok. 1998-2001 között a Pécsi Szülészeti és Nőgyógyászati Klinikán
+  töltöttem, a szülésznőképzés alatt a gyakorlatom. Szakképesítésem megszerzése
+  után, és azóta is ugyanitt dolgozom a szülőszoba részlegen. Az elmúlt évek
+  alatt számtalan konferencián, és továbbképzésen vettem részt, melyek segítenek
+  a biztonságos, és szép szülésélmények megvalósításában. Célom továbbra is,
+  hogy új lehetőségeket, technikákat ismerjek meg, mellyel a vajjúdás és szülés
+  folyamatát tudom segíteni, legyen az háborítatlan, tervezett, hagyományos vagy
+  gyengéd születés. Fontos számomra a várandósok segítése, felkészítése a
+  szülésre. A szülés kísérése, és hogy a családnak ezt a csodálatos eseményt,
+  segítségemmel még szebbé tegyem. Gyermekágyas osztályon töltött munkám során,
+  a Baba-barát kórház cím megszerzése érdekében laktációs elméleti, és
+  gyakorlati képzést, és tapasztalatot is szereztem. 2020-ban Egészségügyi
+  Gyakorlatvezető szakképesítés megszerzését követően, a szülőszobára érkező
+  tanulókat igyekszem pozitív élményekkel, és érzésekkel motiválni. Számukra is
+  hangsúlyozom a segíteni akarást, és az empátia fontosságát.
+</p>
+<h2>Kaizer Melinda</h2>
+<!-- Kárász Melinda képe -->
+
+<p>
+  Az elmúlt 25 évben a Pécsi Szülészeti és Nőgyógyászati Klinika szülőszobáján
+  dolgozva szereztem meg azt a tapasztalatot melyben segíthetem az édesanyákat,
+  hogy a szülést a legcsodálatosabb élményként éljék meg. Az elmúlt hosszú évek
+  alatt sokat változott a szülészeti ellátás és vele együtt változott a várandós
+  édesanyák igénye is a családközpontú háborítatlan szülések irányába. Ezért
+  folyamatos szakmai képzéseken vettem részt és az új ismereteket igyekeztem a
+  szülés kisérések során alkalmazni. Melynek fontos mérföldköve volt a
+  perinatális szaktanácsadó képzés. Folyamatosan tartok személyre szabott és
+  csoportos szülésfelkészítéseket, hogy ne maradjanak megválaszolatlan kérdések
+  vagy kételyek senkiben és egy bizalmi együttműködés jöhessen létre. Fontos
+  számomra a természetes szülés kísérése, a biztonságot mindig szem előtt
+  tartva. Szülésznőként az egyik legszebb és legmeghatározóbb pillanataikban
+  segíthetem az édesanyákat, fizikai és lelki támaszként egyaránt. Hiszem, hogy
+  a pozitív szülés élmény mindannyiunk közös célja.
+</p>
