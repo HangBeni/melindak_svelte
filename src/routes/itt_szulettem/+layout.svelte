40a15f815dbd743d9ccccc87af4ece14736b3608
HangBeni <hangbenjamin2003@gmail.com>
2024-07-08 15:59:25 +0200

    story layout

---
+++ src/routes/itt_szulettem/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+ import {links} from "$lib/utils/links";
+</script>
+<h2>
+    Történetek amik itt születtek
+</h2>
+<p><em>A szülésfelkészítés keretein belül született gyermekek és édasannyuk történeteit olvashatjátok itt</em></p>
+
+<aside>
+ {#each links as link}
+    <a href={link.url}>{link.name}</a>
+  {/each}
+</aside>
+<slot/>
