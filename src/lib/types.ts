5b0da66a50aac8e392c405b65a5b451f9ca0440d
HangBeni <hangbenjamin2003@gmail.com>
2024-07-08 15:56:28 +0200

    moved

--- src/lib/types.ts
+++
@@ -1,4 +0,0 @@
-export type Links = {
-    name: string,
-    url: string
-}
---
+++ src/lib/utils/links.ts
@@ -0,0 +1,8 @@
+import type { Links } from "$lib/utils/types";
+
+export let links : Links[] = [
+    { name: "Főoldal", url: "/" },
+    { name: "Rólunk", url: "/magunkrol" },
+    { name: "Időpontfogalás", url: "/foglalas" },
+    {name: "Itt születtem történeteket", url: "/itt_szulettem"}
+  ];
