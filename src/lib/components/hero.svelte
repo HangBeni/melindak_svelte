cddd21fae4f03896b33404b8d069c85b9b0972bb
HangBeni <hangbenjamin2003@gmail.com>
2024-07-08 15:57:40 +0200

    hero for home page

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -3,5 +3,7 @@ import nappali from "$lib/assets/kepek/IMG_20230121_093747.jpg";
 </script>
 
 <div class="hero">
+  <h2>Melindák szülésfelkészítés</h2>
+  <span><em>"...hogy a kismamáknak biztos támaszt nyújtsunk a várandosságuk alatt..."</em></span>
   <img src={nappali} alt="nappali" />
 </div>
