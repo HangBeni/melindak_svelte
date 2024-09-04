257f892d16b52475b110da11ebe4e2fc423b1f52
HangBeni <hangbenjamin2003@gmail.com>
2024-09-04 18:11:09 +0200

    display the stories

--- src/routes/itt_szulettem/+layout.svelte
+++ src/routes/itt_szulettem/+layout.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
   import NewStory from "$lib/components/new_story.svelte";
-  import { links } from "$lib/utils/links";
   import type { LayoutData } from "./$types";
   import "$lib/styles/index.css";
-  import { count } from "drizzle-orm";
+    import { links } from "$lib/utils/links";
 
   export let data: LayoutData;
 </script>
 
 <h2>Történetek amik itt születtek</h2>
-<p>
+<p id="description">
   <em
     >A szülésfelkészítés keretein belül született gyermekek és édasannyuk
     történeteit olvashatjátok itt</em
@@ -17,18 +16,21 @@
 </p>
 
 <aside>
+
   <nav>
     {#each links.slice(0, 3) as link}
       <a href={link.url}>{link.name}</a>
     {/each}
   </nav>
+
   <div>
     <p>Kismama történetek: <br /> 
     
     {#if typeof data.count === "number" && data.count > 0}
-       {data.count}
+       {data.data?.map((story) => story.id).join(", ")}  
+       <!-- //egy compenensbe integrálni -->
       {:else}
-      "Még nincsenek sztorik!  "
+      Még nincsenek sztorik!
     {/if}  
     
     </p>
@@ -38,23 +40,63 @@
 <slot />
 
 <style>
-  aside {
-    position: fixed;
-    top: 1rem;
-    right: 1rem;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-    background-color: rgba(255, 192, 203, 0.8);
-    border-radius: 0.5rem;
-    padding: 0.5rem;
-    box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.2);
+  
+  @media (max-width: 680px) {
+    aside {
+      position: fixed;
+      top: 2rem;
+      right: 1rem;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      background-color: rgba(255, 192, 203, 0.8);
+      border-radius: 0.5rem;
+      padding: 0.5rem;
+      box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.2);
+    }
+
+    aside a {
+      display: block;
+      margin-bottom: 0.5rem;
+      text-decoration: none;
+    }
+    h2 {
+      margin-left: 2rem;
+    }
+
+    #description {
+      margin-left: 1.35rem;
+      min-width: 10rem;
+      width: 42%;
+      
+      text-align: justify;
+    }
   }
 
-  aside a {
-    display: block;
-    margin-bottom: 0.5rem;
-    text-decoration: none;
-  }
+  @media (min-width: 681px) {
+    aside {
+      position: fixed;
+      top: 2rem;
+      right: 2rem;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      background-color: rgba(255, 192, 203, 0.8);
+      border-radius: 0.5rem;
+      padding: 0.5rem;
+      box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.2);
+      max-width: 300px;
+    }
 
+    h2 {
+      margin-left: 2rem;
+    }
+
+    #description {
+      font-size: larger;
+      margin-left: 1.5rem;
+      text-align: justify;
+      width: 30%;
+    }
+  }
   </style>
