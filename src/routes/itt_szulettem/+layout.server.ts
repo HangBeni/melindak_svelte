df0db5b23c7c5801965174faaa51bdba234c7a2a
HangBeni <hangbenjamin2003@gmail.com>
2024-09-04 18:10:56 +0200

    server pull stories

--- src/routes/itt_szulettem/+layout.server.ts
+++ src/routes/itt_szulettem/+layout.server.ts
@@ -1,16 +1,19 @@
 import { supabase } from "$lib/utils/supabase";
+import type { Stories } from "$lib/utils/types";
 import type { LayoutServerLoad } from "./$types";
 export const load: LayoutServerLoad = async () => {
     const { data, error } = await supabase
         .from('stories')
-        .select('id', { count: 'exact' });
+        .select('id');
     if (error) {
         console.error(error);
         return {
-            count: 0
+            count: 0,
+            data:null
         }
     }
     return {
-        count: data
+        count: data.length,
+        data: data as Stories[],
     }
 };
