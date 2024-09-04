67f0b378f217ab40ac27a7f4a3a807717c0b2e47
HangBeni <hangbenjamin2003@gmail.com>
2024-09-04 18:10:34 +0200

    stories and appointments types in

--- src/lib/utils/types.ts
+++ src/lib/utils/types.ts
@@ -2,3 +2,22 @@ export type Links = {
     name: string,
     url: string
 }
+
+export type Stories = {
+    id: number
+    created_at: string,
+    name: string,
+    story: string,
+    email: string,
+    telephone: string
+}
+
+export type Appointments = {
+    id: number
+    reserved_at: string,
+    name: string,
+    weeks: number,
+    appointment_date: string,
+    contact: string
+
+}
