a0272991eb1e9bfed207d94b6284716feba57ea1
HangBeni <hangbenjamin2003@gmail.com>
2024-09-09 18:09:02 +0200

    refactored base appointment

---
+++ src/lib/components/appointment/base.svelte
@@ -0,0 +1,26 @@
+<div class="content-container">
+    <div class="container">
+        <h1>Az időpontot telefonon is lehet egyeztetni!</h1>
+        <table>
+            <tr>
+                <th>Szolgáltatásaink</th>
+                <th>Áraink</th>
+            </tr>
+            <tr>
+                <td>Első alkalom</td>
+                <td class="price">20 000 Ft</td>
+            </tr>
+            <tr>
+                <td>További konzultációk</td>
+                <td class="price">15 000 Ft</td>
+            </tr>
+        </table>
+
+        <p>Az első alkalom ára tartalmazza a korlátlan telefonos segítségnyújtást</p>
+
+        <div class="contact">
+            <p>Kaizer Melinda elérhetőségei -> kaizermelinda@gmail.com +36 20 451 7007 <br>16:00 után elérhető!</p>
+            <p>Právicz-Kárász Melinda elérhetőségei -> karaszmelinda2@gmail.com +36 20 210 1749</p>
+        </div>
+    </div>
+</div>
