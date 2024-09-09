072e32163d7cb908f16e7ca9913c7c43ccea5d2f
HangBeni <hangbenjamin2003@gmail.com>
2024-09-09 18:10:11 +0200

    refactor

--- src/routes/(app)/foglalas/+page.svelte
+++ src/routes/(app)/foglalas/+page.svelte
@@ -1,29 +1,10 @@
 <script lang="ts">
+    import Base from "$lib/components/appointment/base.svelte";
+    import NewAppointment from "$lib/components/forms/new_appointment.svelte";
+    import type { PageServerData } from "./$types";
+
+    export let data: PageServerData;
 </script>
 
-<div class="content-container">
-    <div class="container">
-        <h1>Az időpontot telefonon lehet egyeztetni!</h1>
-        <table>
-            <tr>
-                <th>Szolgáltatásaink</th>
-                <th>Áraink</th>
-            </tr>
-            <tr>
-                <td>Első alkalom</td>
-                <td class="price">20 000 Ft</td>
-            </tr>
-            <tr>
-                <td>További konzultációk</td>
-                <td class="price">15 000 Ft</td>
-            </tr>
-        </table>
-
-        <p>Az első alkalom ára tartalmazza a korlátlan telefonos segítségnyújtást</p>
-
-        <div class="contact">
-            <p>Kaizer Melinda elérhetőségei -> kaizermelinda@gmail.com +36 20 451 7007 <br>16:00 után elérhető!</p>
-            <p>Právicz-Kárász Melinda elérhetőségei -> karaszmelinda2@gmail.com +36 20 210 1749</p>
-        </div>
-    </div>
-</div>
+<Base />
+<NewAppointment appointments={data.appointments}/>
