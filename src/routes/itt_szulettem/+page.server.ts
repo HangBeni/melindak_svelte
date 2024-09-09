b584b17bdcc6762dc401c7fde0156ebc0861ec3f
HangBeni <hangbenjamin2003@gmail.com>
2024-09-09 18:10:39 +0200

    post a new story

---
+++ src/routes/itt_szulettem/+page.server.ts
@@ -0,0 +1,41 @@
+import { supabase } from "$lib/utils/supabase";
+import type { Stories } from "$lib/utils/types";
+import type { Actions } from "./$types";
+
+
+export const load = async () => {
+    const { data, error } = await supabase
+        .from('stories')
+        .select('*')
+        .eq('public', true);
+
+    if (error) {
+        console.error(error);
+        return {
+            stories: [] as Stories[]
+        }
+    }
+
+    return {
+        stories: data as Stories[]
+    }
+}
+
+export const actions = {
+    story: async ({ request }) => {
+        const data = await request.formData();
+
+        const { error } = await supabase.from('stories').insert({
+            created_at: new Date().toISOString(),
+            name: data.get('name'),
+            story: data.get('story'),
+            email: data.get('email'),
+            phone: data.get('telephone'),
+            public: false
+        });
+
+        if (error) {
+            console.error(error);
+        }
+    }
+} satisfies Actions
