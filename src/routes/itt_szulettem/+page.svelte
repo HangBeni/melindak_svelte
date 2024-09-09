4cc02952f2a4766b8252e55302c87179494fa3c6
HangBeni <hangbenjamin2003@gmail.com>
2024-09-09 18:10:52 +0200

    display the stories

--- src/routes/itt_szulettem/+page.svelte
+++ src/routes/itt_szulettem/+page.svelte
@@ -1,3 +1,29 @@
+<script lang="ts">
+    import StoryCard from "$lib/components/birth/story_card.svelte";
+    import type { PageServerData } from "./$types";
 
+
+
+export let data: PageServerData;
+</script>
 
 <!--történetek felsorolása adatbázisból-->
+<main>
+{#each data.stories as story}
+    <StoryCard {story} />
+{/each}
+</main>
+
+
+<style>
+    main{
+        display: grid;
+        grid-template-columns: repeat(minmax(300px, 1fr), auto-fit);
+        margin-inline: auto;
+        margin-top: 10rem;
+        width: 50rem;
+        padding: 0.75rem;
+        background-color: rgba(255, 192, 203, 0.88);
+        border-radius: 0.75rem;
+    }
+</style>
