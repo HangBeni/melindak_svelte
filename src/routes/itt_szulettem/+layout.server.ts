8b5929f994e7d94258ad6eb129f2e1ed09ab2021
HangBeni <hangbenjamin2003@gmail.com>
2024-09-11 19:28:13 +0200

    get the number of real stories

--- src/routes/itt_szulettem/+layout.server.ts
+++ src/routes/itt_szulettem/+layout.server.ts
@@ -4,7 +4,7 @@ import type { LayoutServerLoad } from "./$types";
 export const load: LayoutServerLoad = async () => {
     const { data, error } = await supabase
         .from('stories')
-        .select('*');
+        .select('*').eq('public', true).order('created_at', { ascending: false });
 
     if (error) {
         console.error(error);
