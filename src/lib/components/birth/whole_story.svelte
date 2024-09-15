b31dacc7628855300b5ff8cc828b11a7e575381a
HangBeni <hangbenjamin2003@gmail.com>
2024-09-15 18:28:38 +0200

    created: the main component for the opened story

---
+++ src/lib/components/birth/whole_story.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import type { Stories } from "$lib/utils/types";
+
+    
+    export let story: Stories;
+</script>
+
+<main>
+    <p>
+        {story.story}
+    </p>
+</main>
+
+<style>
+
+
+</style>
+
