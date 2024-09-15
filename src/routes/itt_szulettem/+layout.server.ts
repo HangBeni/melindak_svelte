a30ee19c692e5c3aa6929b0b57249c0e493a0cf1
HangBeni <hangbenjamin2003@gmail.com>
2024-09-15 18:28:58 +0200

    no need for data or Stories type

--- src/routes/itt_szulettem/+layout.server.ts
+++ src/routes/itt_szulettem/+layout.server.ts
@@ -1,5 +1,4 @@
 import { supabase } from "$lib/utils/supabase";
-import type { Stories } from "$lib/utils/types";
 import type { LayoutServerLoad } from "./$types";
 export const load: LayoutServerLoad = async () => {
     const { data, error } = await supabase
@@ -9,8 +8,7 @@ export const load: LayoutServerLoad = async () => {
     if (error) {
         console.error(error);
         return {
-            count: 0,
-            data:null
+            count: 0
         }
     }
     return {
