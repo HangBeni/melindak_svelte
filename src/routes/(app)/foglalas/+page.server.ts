01c99fe7d3d376e4a7ed6e8dffea69917110c6d2
HangBeni <hangbenjamin2003@gmail.com>
2024-09-16 19:06:22 +0200

    feat: get the booked and av. appointments, post new appointment

--- src/routes/(app)/foglalas/+page.server.ts
+++ src/routes/(app)/foglalas/+page.server.ts
@@ -1,16 +1,42 @@
 import { supabase } from "$lib/utils/supabase";
-import type { Appointments } from "$lib/utils/types";
+import type { Appointments, AppointmentTimes } from "$lib/utils/types";
+import type { Action, Actions } from "./$types";
 
 
 export const load = async () => {
-  const { data, error } = await supabase.from('appointments').select('*');
-  if (error) {
-    console.error(error);
+  const { data:booked, error:booked_error } = await supabase.from('appointments').select('*');
+  const { data:available, error:available_error } = await supabase.from('appointments').select('*');
+  if (booked_error || available_error) {
+    console.error("Booked: "+ booked_error);
+    console.error("Available: "+ available_error);
     return {
-      appointments: [] as Appointments[]
+      booked_appointments: [] as Appointments[],
+    appointment_times: [] as AppointmentTimes[]
     }
   }
   return {
-    appointments: data as Appointments[]
+    booked_appointments: booked as Appointments[],
+    appointment_times: available as AppointmentTimes[]
+  }
+};
+
+
+export const actions: Actions = {
+
+  book: async ({ request }) => {
+
+    const data = await request.formData();
+
+    const { error } = await supabase.from('appointments').insert({
+      name: data.get('name'),
+      weeks: data.get('weeks'),
+      appointment_time: data.get('time'),
+      email: data.get('email'),
+      phone: data.get('phone'),
+      melinda: data.get('melinda')
+    });
+    if (error) {
+      console.error(error);
+    }
   }
 };
