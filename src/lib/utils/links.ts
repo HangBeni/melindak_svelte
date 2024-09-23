eeea5816e7a75aba01b409ce4be84cad327e0c97
HangBeni <85244745+HangBeni@users.noreply.github.com>
2024-09-23 18:09:10 +0200

    helyesírás

--- src/lib/utils/links.ts
+++ src/lib/utils/links.ts
@@ -4,5 +4,5 @@ export let links : Links[] = [
     { name: "Főoldal", url: "/" },
     { name: "Rólunk", url: "/magunkrol" },
     { name: "Időpontfogalás", url: "/foglalas" },
-    {name: "Itt születtem történeteket", url: "/itt_szulettem"}
+    {name: "\"Itt születtem\" történetek", url: "/itt_szulettem"}
   ];
