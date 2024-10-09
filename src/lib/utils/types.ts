1c6feba897afb32b6b771ed90606ea3189fea5a1
HangBeni <hangbenjamin2003@gmail.com>
2024-10-09 20:24:47 +0200

    removed the appointments

--- src/lib/utils/types.ts
+++ src/lib/utils/types.ts
@@ -4,28 +4,12 @@ export type Links = {
 }
 
 export type Stories = {
-    id: number
+    id?: number
     created_at: string,
     name: string,
-    story: string,
-    email: string,
+    story?: string,
+    email?: string,
     telephone: string,
-    public: boolean
-}
-
-export type Appointments = {
-    id?: number,
-    reserved_at?: string,
-    name: string,
-    weeks: number,
-    appointment_time: string,
-    email: string,
-    phone: string,
-    melinda: string
-}
-
-export type AppointmentTimes = {
-    id: number,
-    created_at: string,
-    time: string
+    public: boolean,
+    picture: string
 }
