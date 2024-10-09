68a406f878e207ce873894231b2ae0b819746c6b
HangBeni <hangbenjamin2003@gmail.com>
2024-10-09 20:25:44 +0200

    upload pics, not finished

--- src/routes/itt_szulettem/+page.server.ts
+++ src/routes/itt_szulettem/+page.server.ts
@@ -2,40 +2,56 @@ import { supabase } from "$lib/utils/supabase";
 import type { Stories } from "$lib/utils/types";
 import type { Actions } from "./$types";
 
-
 export const load = async () => {
-    const { data, error } = await supabase
-        .from('stories')
-        .select('*')
-        .eq('public', true);
+  const { data, error } = await supabase
+    .from("stories")
+    .select("*")
+    .eq("public", true);
 
-    if (error) {
-        console.error(error);
-        return {
-            stories: [] as Stories[]
-        }
-    }
-
+  if (error) {
+    console.error(error);
     return {
-        stories: data as Stories[]
-    }
-}
+      stories: [] as Stories[],
+    };
+  }
+
+  return {
+    stories: data as Stories[],
+  };
+};
 
 export const actions = {
-    story: async ({ request }) => {
-        const data = await request.formData();
-
-        const { error } = await supabase.from('stories').insert({
-            created_at: new Date().toISOString(),
-            name: data.get('name'),
-            story: data.get('story'),
-            email: data.get('email'),
-            phone: data.get('telephone'),
-            public: false
-        });
-
-        if (error) {
-            console.error(error);
-        }
+  story: async ({ request }) => {
+    const data = await request.formData();
+    const picture = data.get("picture") as string;
+    console.log(picture.trim());
+    const picExt = picture.trim().split(".").pop();
+    const path = `${Math.round(Math.random() * 100000)}.${picExt}`;
+    const { error: picError } = await supabase.storage
+      .from("story_pics")
+      .upload(path, picture);
+    if (picError) {
+      throw picError;
     }
-} satisfies Actions
+
+    const { data: downloadPicturePath, error: downloadError } =
+      await supabase.storage.from("story_pics").download(path);
+    if (downloadError) {
+      throw downloadError;
+    }
+
+    const pictureURL = URL.createObjectURL(downloadPicturePath);
+    const { error } = await supabase.from("stories").insert({
+      created_at: new Date().toISOString(),
+      name: data.get("name"),
+      story: data.get("story"),
+      email: data.get("email"),
+      phone: data.get("telephone"),
+      picture: pictureURL,
+    });
+
+    if (error) {
+      console.error(error);
+    }
+  },
+} satisfies Actions;
