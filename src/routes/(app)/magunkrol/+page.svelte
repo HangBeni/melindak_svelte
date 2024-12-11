d6bf7d390efabf684706bfcf3469617c56efe119
HangBeni <hangbenjamin2003@gmail.com>
2024-12-11 15:05:30 +0100

    removed: Kaizer Melinda kilépett

--- src/routes/(app)/magunkrol/+page.svelte
+++ src/routes/(app)/magunkrol/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import karasz_melinda from "$lib/assets/kepek/Karasz_M.jpg";
-  import kaizer_melinda from "$lib/assets/kepek/Kaizer_M.jpg";
 </script>
 
 <div class="content-container">
@@ -37,43 +36,11 @@
         hangsúlyozom a segíteni akarást, és az empátia fontosságát.
       </p>
     </article>
-
-    <article>
-      <h2>Kaizer Melinda</h2>
-      <img src={kaizer_melinda}
-      class="imgbasic img-left"
-      alt="Kárász Melinda képe" />
-      <p>
-        Az elmúlt 25 évben a Pécsi Szülészeti és Nőgyógyászati Klinika
-        szülőszobáján dolgozva szereztem meg azt a tapasztalatot melyben
-        segíthetem az édesanyákat, hogy a szülést a legcsodálatosabb élményként
-        éljék meg.
-      </p>
-      <p>
-        Az elmúlt hosszú évek alatt sokat változott a szülészeti ellátás és vele
-        együtt változott a várandós édesanyák igénye is a családközpontú
-        háborítatlan szülések irányába. Ezért folyamatos szakmai képzéseken
-        vettem részt és az új ismereteket igyekeztem a szülés kisérések során
-        alkalmazni. Melynek fontos mérföldköve volt a perinatális szaktanácsadó
-        képzés.
-      </p>
-      <p>
-        Folyamatosan tartok személyre szabott és csoportos
-        szülésfelkészítéseket, hogy ne maradjanak megválaszolatlan kérdések vagy
-        kételyek senkiben és egy bizalmi együttműködés jöhessen létre. Fontos
-        számomra a természetes szülés kísérése, a biztonságot mindig szem előtt
-        tartva. Szülésznőként az egyik legszebb és legmeghatározóbb
-        pillanataikban segíthetem az édesanyákat, fizikai és lelki támaszként
-        egyaránt.
-      </p>
-      <p>Hiszem, hogy a pozitív szülés élmény mindannyiunk közös célja.</p>
-    </article>
   </div>
 </div>
 
-
 <style>
   .content-container {
-  margin-bottom: 2rem;
+    margin-bottom: 2rem;
   }
 </style>
